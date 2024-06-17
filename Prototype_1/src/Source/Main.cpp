e35dfe53fc36cc4336a19d2110a09346147038ca
xafras <teddy88.boulanger@gmail.com>
2024-06-17 15:24:34 +0200

    ajout de sfml audio avec un son de test au démarrage

--- Prototype_1/src/Source/Main.cpp
+++ Prototype_1/src/Source/Main.cpp
@@ -1,7 +1,16 @@
 #include "Game.hpp"
+#include <SFML/Audio.hpp>
 
 int main()
 {
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("media/harp.wav"))
+        return -1;
+    sf::Sound sound;
+    sound.setBuffer(buffer);
+    sound.play();
+
 	Game game;
     game.run();
+    return 0;
 }
