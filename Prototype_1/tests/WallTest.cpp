4d6d58c3cfead822db6eca791d5638f861cf02a5
xafras <teddy88.boulanger@gmail.com>
2024-06-18 23:42:52 +0200

    added unit test

--- Prototype_1/tests/WallTest.cpp
+++ Prototype_1/tests/WallTest.cpp
@@ -41,10 +41,7 @@ namespace Wall_test {
 			grp->update(elapsedTime, altView, textures);
 			grp->collide(grp->getPos(), grp->getSiz(), elapsedTime, true);
 			EXPECT_EQ(grp->getPos().x, position_initiale);
-
-			
 		}
-
     }
 
 	TEST(WallTest, playerOnLeft_noCollision) {
@@ -78,10 +75,98 @@ namespace Wall_test {
 			grp->update(elapsedTime, altView, textures);
 			grp->collide(grp->getPos(), grp->getSiz(), elapsedTime, true);
 			EXPECT_EQ(grp->getPos().x, position_initiale + PlayerSpeed * elapsedTime.asSeconds());
-
-
 		}
-
 	}
 
+	TEST(WallTest, playerOnRight_collision) {
+		std::string sc = R"(<?xml version="1.0"?>
+							<Monde>
+								<Niveau label="M1_Niveau1">
+									<!-- Crée un élément en position (x,y) de taille (w,h) -->     
+									<Player x="0" y="-106" h="106" w="144"/>
+									<Pacifier x="-420" y="-196" h="196" w="184"/>
+									<Wall x="0" y="0" h="168" w="512" vertical="false"/>
+									<Wall x="-512" y="-700" h="700" w="512" vertical="true"/>
+								</Niveau>
+							</Monde>)";
+		pugi::xml_document doc;
+		pugi::xml_parse_result result = doc.load_string(sc.c_str());
+		ASSERT_TRUE(result) << result.description();
+
+		for (auto const& child : doc.child("Monde"))
+		{
+			auto grp = std::make_unique<Group>(child);
+			float position_initiale = grp->getPos().x;
+			EXPECT_EQ(grp->getPos().x, position_initiale);
+
+			grp->handlePlayerInput(sf::Keyboard::Q, false);
+			grp->update(elapsedTime, altView, textures);
+			grp->collide(grp->getPos(), grp->getSiz(), elapsedTime, true);
+			EXPECT_EQ(grp->getPos().x, position_initiale);
+
+			grp->handlePlayerInput(sf::Keyboard::Q, true);
+			grp->update(elapsedTime, altView, textures);
+			grp->collide(grp->getPos(), grp->getSiz(), elapsedTime, true);
+			EXPECT_EQ(grp->getPos().x, position_initiale);
+		}
+	}
+
+	TEST(WallTest, playerOnRight_noCollision) {
+		std::string sc = R"(<?xml version="1.0"?>
+							<Monde>
+								<Niveau label="M1_Niveau1">
+									<!-- Crée un élément en position (x,y) de taille (w,h) -->     
+									<Player x="0" y="-106" h="106" w="144"/>
+									<Pacifier x="-420" y="-196" h="196" w="184"/>
+									<Wall x="0" y="0" h="168" w="512" vertical="false"/>
+									<Wall x="-550" y="-700" h="700" w="512" vertical="true"/>
+								</Niveau>
+							</Monde>)";
+		pugi::xml_document doc;
+		pugi::xml_parse_result result = doc.load_string(sc.c_str());
+		ASSERT_TRUE(result) << result.description();
+
+		for (auto const& child : doc.child("Monde"))
+		{
+			auto grp = std::make_unique<Group>(child);
+			float position_initiale = grp->getPos().x;
+			EXPECT_EQ(grp->getPos().x, position_initiale);
+
+			grp->handlePlayerInput(sf::Keyboard::Q, false);
+			grp->update(elapsedTime, altView, textures);
+			grp->collide(grp->getPos(), grp->getSiz(), elapsedTime, true);
+			EXPECT_EQ(grp->getPos().x, position_initiale);
+
+			grp->handlePlayerInput(sf::Keyboard::Q, true);
+			grp->update(elapsedTime, altView, textures);
+			grp->collide(grp->getPos(), grp->getSiz(), elapsedTime, true);
+			EXPECT_EQ(grp->getPos().x, position_initiale - PlayerSpeed * elapsedTime.asSeconds());
+		}
+	}
+
+	TEST(WallTest, playerAbove_noCollision) {
+		std::string sc = R"(<?xml version="1.0"?>
+							<Monde>
+								<Niveau label="M1_Niveau1">
+									<!-- Crée un élément en position (x,y) de taille (w,h) -->     
+									<Player x="0" y="0" h="106" w="144"/>
+									<Pacifier x="-420" y="-196" h="196" w="184"/>
+								</Niveau>
+							</Monde>)";
+		pugi::xml_document doc;
+		pugi::xml_parse_result result = doc.load_string(sc.c_str());
+		ASSERT_TRUE(result) << result.description();
+
+		for (auto const& child : doc.child("Monde"))
+		{
+			auto grp = std::make_unique<Group>(child);
+			float position_initiale = grp->getPos().y;
+			EXPECT_EQ(grp->getPos().y, position_initiale);
+
+			grp->handlePlayerInput(sf::Keyboard::S, true);
+			grp->update(elapsedTime, altView, textures);
+			grp->collide(grp->getPos(), grp->getSiz(), elapsedTime, true);
+			EXPECT_TRUE(grp->getPos().y > position_initiale);
+		}
+	}
 }
